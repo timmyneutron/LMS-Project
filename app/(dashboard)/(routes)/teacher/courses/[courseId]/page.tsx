7ca58f2afc4fc8dda2fd912eb598b277afd75274
Tim Abrahamsen <timmy_neutron@mac.com>
2025-02-01 19:20:39 -0500

    course id page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,8 +1,36 @@
-const CourseIdPage = ({
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({
   params
 }: {
   params: { courseId: string }
 }) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId
+    }
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
   return ( <div>Course ID: {params.courseId}</div> );
 }
  
