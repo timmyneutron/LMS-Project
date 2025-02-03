54122e459960ae5122a8d4a0c8ac23812d298219
Tim Abrahamsen <timmy_neutron@mac.com>
2025-02-02 21:33:28 -0500

    category form (doesn't work)

---
+++ scripts/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+
+const db = new PrismaClient();
+
+async function main() {
+  try {
+    await db.category.createMany({
+      data: [
+        { name: "Computer Science" },
+        { name: "Music" },
+        { name: "Fitness" },
+        { name: "Photography" },
+        { name: "Accounting" },
+        { name: "Engineering" },
+        { name: "Filming" },
+      ]
+    });
+
+    console.log("Success");
+  } catch (error) {
+    console.log("Error seeding the database categories", error);
+  } finally {
+    await db.$disconnect();
+  }
+}
+
+main();
