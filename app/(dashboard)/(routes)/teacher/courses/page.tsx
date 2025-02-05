c4935d62ba7c7e7cdd8f6d1036ec56df00cc3310
Tim Abrahamsen <timmy_neutron@mac.com>
2025-02-05 15:54:51 -0500

    courses data table

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,5 +1,33 @@
-const CoursesPage = () => {
-  return ( <div>Courses Page</div> );
+import { auth } from "@clerk/nextjs/server";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+
+const CoursesPage = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <DataTable
+        columns={columns}
+        data={courses}
+      />
+    </div>
+  );
 }
  
 export default CoursesPage;
