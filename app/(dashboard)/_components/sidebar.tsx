48a93ba3d1121d0cc6adbd1ee3efe8bf94445170
Tim Abrahamsen <timmy_neutron@mac.com>
2025-02-12 18:24:56 -0500

    fix sidebar layout

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -4,7 +4,7 @@ import SidebarRoutes from "./sidebar-routes";
 const Sidebar = () => {
   return (
   <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-    <div className="p-6">
+    <div className="h-[80px] flex items-center justify-center">
       <Logo />
     </div>
     <div className="flex flex-col w-full">
