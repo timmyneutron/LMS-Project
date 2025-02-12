5a75adb8e183656ce66aa4686c42c460f79dc9cb
Tim Abrahamsen <timmy_neutron@mac.com>
2025-02-12 10:16:08 -0500

    protect teacher routes

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { useAuth, UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 import { SearchInput } from "./search-input";
+import { isTeacher } from "@/lib/teacher";
 
 export const NavbarRoutes = () => {
+  const { userId } = useAuth();
   const pathName = usePathname();
 
   const isTeacherPage = pathName?.startsWith("/teacher");
@@ -22,20 +24,24 @@ export const NavbarRoutes = () => {
         </div>
       )}
       <div className="flex gap-x-2 ml-auto">
-        { isTeacherPage || isCoursePage ? (
+        { isTeacherPage || isCoursePage ?
+        (
           <Link href="/">
             <Button>
               <LogOut className="h-4 w-4 mr-2"/>
               Exit
             </Button>
           </Link>
-        ) : (
-          <Link href="/teacher/courses">
-            <Button size="sm" variant="ghost">
-              Teacher mode
-            </Button>
-          </Link>
-        )}
+        ) : isTeacher(userId) ?
+          (
+            <Link href="/teacher/courses">
+              <Button size="sm" variant="ghost">
+                Teacher mode
+              </Button>
+            </Link>
+          ) : (
+            null
+          )}
         <UserButton />
       </div>
     </>
